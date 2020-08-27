7fa8562111df57f7ee1b0ba003d9fa56c5e0e841
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-27 19:03:21 +0530

    Datch National Flag Algorithm

---
+++ SDE-PROBLEMS-30Days/Day1/Sort_Array_Of_0s_1s_2s_Without_Using_DATCH_NATIONAL_FLAG_ALGO.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Sort_Array_Of_0s_1s_2s_Without_Using_DATCH_NATIONAL_FLAG_ALGO {
+
+	static int[] arr = new int[] {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}; // Given Array
+
+	static void swap(int[] arr, int i, int j) {
+		int temp;
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public static void main(String[] args) {
+
+		int low, mid, high; // Apply Datch National Flag Algorithm: TC:O(n),AS:O(1)
+		low = 0;
+		mid = 0;
+		high = arr.length - 1;
+
+
+		while (mid <= high) {
+			switch (arr[mid]) {
+
+			// If the element is 0.
+			case 0:
+				swap(arr, low++, mid++);
+				break;
+
+			// If the element is 1.
+			case 1:
+				mid++;
+				break;
+
+			// If the element is 2.
+			case 2:
+				swap(arr, mid, high--);
+				break;
+			}
+		}
+
+		for (int i : arr)
+			System.out.print(i + "  ");
+
+	}
+}
