805fdaba011ffc90c1284d8190839a3abab65c7f
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-26 23:59:52 +0530

    Best optimal approach

--- SDE-PROBLEMS-30Days/Day4/Four_Sum_Problem.java
+++ SDE-PROBLEMS-30Days/Day4/Four_Sum_Problem.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
-
-public class Four_Sum_Problem {
-
+/*
+class Solution { // O(n^4) Time complexity & O(n^2) Auxiliary Space solution
    public static ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-    // O(n^4) Time complexity & O(n^2) Auxiliary Space solution
+    
     		int N = num.length;
     		Arrays.sort(num);
 	    	HashSet<ArrayList<Integer>> hSet = new HashSet<ArrayList<Integer>>();
@@ -34,13 +33,73 @@ public class Four_Sum_Problem {
 		    }
     	return result;
 	}
+}
+*/
 
+
+
+class Solution { // O(n^3) Time complexity & O(n) Auxiliary Space solution
+   public static ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+    		
+    		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+    		if(num == null || num.length==0)
+    			return result;
+    		int N = num.length;
+    		Arrays.sort(num);
+	    	
+		    
+		    for (int i = 0; i < N; i++) {
+		        for (int j = i + 1; j < N; j++) {
+		            	
+		            	int target_2 = target - num[i] - num[j]; // remaining target value
+		            	
+		            	int left = j+1;
+		            	int right = N-1;
+
+			                while (left < right) {
+			                	// using two sum problem 
+			                	int two_sum = num[left] + num[right];  
+
+			                	if(two_sum < target_2) left++;
+
+			                	else if(two_sum > target_2) right--;
+
+			                	else{
+				                    ArrayList<Integer> quad = new ArrayList<Integer>();
+				                    quad.add(num[i]);
+				                    quad.add(num[j]);
+				                    quad.add(num[left]);
+				                    quad.add(num[right]);
+
+				                    result.add(quad);
+
+				                    //Processing the duplicates of number 3
+				                    while( left < right && num[left] == quad.get(2)) ++left;
+
+				                    //Processing the duplicates of number 4
+				                    while(left < right && num[right] == quad.get(3)) --right;
+
+				                }
+			            	}
+			        //Processing the duplicates of number 2
+			        while(j+1 < N && num[j+1] == num[j]) ++j;        	
+		        }
+		        	//Processing the duplicates of number 1
+		        	while(i+1 < N && num[i+1] == num[i]) ++i;
+		    }
+    	return result;
+	}
+}
+
+public class Four_Sum_Problem {
 	public static void main(String[] args) {
 		int n = 8;
 		int[] nums = new int[]{1,2,1,3,4,3,1,4,2,4};
 		int target = 9;
 
-		ArrayList<ArrayList<Integer>> arr = fourSum(nums, target);
+		Solution obj = new Solution();
+		ArrayList<ArrayList<Integer>> arr = obj.fourSum(nums, target);
 
 		System.out.println("The unique four sum quadruplets are:");
 		for(int i=0;i<arr.size();i++){
