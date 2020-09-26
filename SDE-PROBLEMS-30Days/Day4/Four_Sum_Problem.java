443f6281abf4c82dbf0a452ebe060667e198da59
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-26 22:34:17 +0530

    brute force approach

--- SDE-PROBLEMS-30Days/Day4/Four_Sum_Problem.java
+++ SDE-PROBLEMS-30Days/Day4/Four_Sum_Problem.java
@@ -1,42 +1,34 @@
 import java.util.*;
 
-/*
+
 public class Four_Sum_Problem {
 
    public static ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-    // O(n^3) Time complexity & O(n^2) Auxiliary Space solution
+    // O(n^4) Time complexity & O(n^2) Auxiliary Space solution
     		int N = num.length;
-	    	Arrays.sort(num);
+    		Arrays.sort(num);
 	    	HashSet<ArrayList<Integer>> hSet = new HashSet<ArrayList<Integer>>();
+	    	
 	    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		    
-		    for (int i = 0; i < N; i++) {
-		        for (int j = i + 1; j < N; j++) {
-		            for (int k = j + 1, l = N - 1; k < l;) { //using two pinter technique
-
-		                int sum = num[i] + num[j] + num[k] + num[l];
-		                
-		                if (sum > target) {
-		                    l--;
-		                }
-		                else if (sum < target) {
-		                    k++;
-		                }
-		                else if (sum == target) {
-		                    ArrayList<Integer> found = new ArrayList<Integer>();
-		                    found.add(num[i]);
-		                    found.add(num[j]);
-		                    found.add(num[k]);
-		                    found.add(num[l]);
-		                    if (!hSet.contains(found)) {
-		                        hSet.add(found);
-		                        result.add(found);
-		                    }
-
-		                    k++;
-		                    l--;
-
-		                }
+		    for (int i = 0; i < N-3; i++) {
+		        for (int j = i + 1; j < N-2; j++) {
+		            for (int k = j + 1; k < N-1; k++) { 
+		            	for(int l = k+1; l < N; l++) {
+			                int sum = num[i] + num[j] + num[k] + num[l];
+			             
+			                if (sum == target) {
+			                    ArrayList<Integer> quad = new ArrayList<Integer>();
+			                    quad.add(num[i]);
+			                    quad.add(num[j]);
+			                    quad.add(num[k]);
+			                    quad.add(num[l]);
+		                    	if (!hSet.contains(quad)) {
+		                        	hSet.add(quad);
+		                        	result.add(quad);
+		                    	}
+			                }
+			            }
 		            }
 		        }
 		    }
@@ -45,7 +37,7 @@ public class Four_Sum_Problem {
 
 	public static void main(String[] args) {
 		int n = 8;
-		int[] nums = new int[]{1, 2, 1, 3, 2, 3, 4, 4, 2};
+		int[] nums = new int[]{1,2,1,3,4,3,1,4,2,4};
 		int target = 9;
 
 		ArrayList<ArrayList<Integer>> arr = fourSum(nums, target);
@@ -56,9 +48,8 @@ public class Four_Sum_Problem {
 				System.out.print(" -> "+arr.get(i).get(j));
 			}
 			System.out.println();
-		}
-		
+		}		
 		
 	}
 }
-*/
+
