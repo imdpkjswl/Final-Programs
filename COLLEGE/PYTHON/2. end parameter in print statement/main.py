f75dcb73f616666156f9ca6e126734c01a602ecc
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-30 01:48:35 +0530

    #2

---
+++ COLLEGE/PYTHON/2. end parameter in print statement/main.py
@@ -0,0 +1,16 @@
+# This is the single line comment
+
+"""
+This is the multiple line comment
+"""
+
+
+print("Hello Everyone","I am Deepak Jaiswal")
+
+print("Hello Everyone", end="")  # end parameter may used to attach two statement in single line
+print("I am Deepak Jaiswal")
+
+print("C:\\hello") # print single slash
+print("C:\"hello") # print double quote slash
+print("C:\'hello") # print single quote
+
