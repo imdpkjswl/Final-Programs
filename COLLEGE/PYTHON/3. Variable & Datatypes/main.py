f3fb97e4fbf2992540ba77f74a1bc165afaa8e97
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-30 02:23:21 +0530

    #3

---
+++ COLLEGE/PYTHON/3. Variable & Datatypes/main.py
@@ -0,0 +1,15 @@
+# Variables
+
+var1 = 'String variable'
+var2 = 112
+var3 = 112.3
+var4 = True
+
+# type is used to find data type/class of variables
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+print(10*"Hello")  # print Hello 10 times in single line
+
