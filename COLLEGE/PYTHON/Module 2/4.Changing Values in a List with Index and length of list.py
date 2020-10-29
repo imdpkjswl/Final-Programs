dbb24f1991ce162fa0ee7a2d73202b92ba996cc6
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-10-29 21:56:10 +0530

    module 2

---
+++ COLLEGE/PYTHON/Module 2/4.Changing Values in a List with Index and length of list.py
@@ -0,0 +1,9 @@
+spam = ['cat', 'bat', 'rat', 'elephant']
+
+# Get length of list
+length = len(spam)
+print(length)
+
+# Assign value at index
+spam[1] = 'dog'
+print(spam)
---
+++ COLLEGE/PYTHON/Module 2/5.List Concatenation and List Replication.py
@@ -0,0 +1,14 @@
+# Concatenation of two list
+ls1 = [1, 2, 3]
+ls2 = ['A', 'B', 'C']
+ls = ls1 + ls2
+print(ls)
+print(ls[3]) #after merged, get value at index 3
+
+# Replication of list
+ls3 = ['X', 'Y', 'Z'] * 3
+print(ls3)
+
+spam = [1, 2, 3]
+spam = spam + ['A', 'B', 'C']
+print(spam)
