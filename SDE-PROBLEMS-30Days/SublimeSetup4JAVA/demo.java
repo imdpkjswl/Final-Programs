625ec4d349c1a7dd1c4fd0664dbb7e51d764ed24
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-11-22 19:12:19 +0530

    sublimesetupforjava

---
+++ SDE-PROBLEMS-30Days/SublimeSetup4JAVA/demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class demo {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int a, b, sum;
+		System.out.println("Enter two number:");
+		a = in.nextInt();
+		b = in.nextInt();
+		sum = a + b;
+		System.out.println("Sum is: " + sum);
+
+	}
+}
