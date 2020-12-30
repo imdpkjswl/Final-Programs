f7d9fdab707dab8f4a6772884bfe0e2912c6d156
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-12-30 23:58:22 +0530

    P2P with three nodes using UDP

--- COLLEGE/CNS LAB/PART-A/LAB1/prog1.tcl
+++ COLLEGE/CNS LAB/PART-A/LAB1/prog1.tcl
@@ -1,85 +1,55 @@
-set ns [new Simulator]   /*object create of Simulator classs */
+#Create a ns simulator
+set ns [new Simulator]
 
-
-# Open nam trace file in write mode
-set nf [open prog1.nam w]
-$ns namtrace-all $nf
-
-
-# Open trace file in write mode
+#Open the NS trace file
 set tf [open prog1.tr w]
 $ns trace-all $tf
 
+#Open the NAM trace file
+set nf [open prog1.nam w]
+$ns namtrace-all $nf
 
-# Finish process
+#Define a 'finish' procedure
 proc finish {} {
-global ns nf tf
-close $nf
+global ns tf nf
+$ns flush-trace
 close $tf
-exec nam prog1.nam & 
+close $nf
+exec nam prog1.nam &
 exit 0
 }
 
 
 
-# Code for creating node
-set n0 [$ns node]
+#Create 3 nodes
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 
 
-
-# Code to label the nodes
-$n0 label "Source/udp0"
-$n1 label "Source/udp1"
-$n2 label "Router"
-$n3 label "Destination/Null"
-
-
-# Vary the bandwidths below and See the change in the number of packets dropped in each Simulation
-$ns duplex-link $n0 $n2 250Kbps 20ms DropTail
-$ns duplex-link $n1 $n2 25Mb 20ms DropTail
-$ns duplex-link $n2 $n3 10Mb 20ms DropTail
+#Createlinks between nodes
+$ns duplex-link $n1 $n2 1Mb 60ms DropTail
+$ns queue-limit $n1 $n2 10
+$ns duplex-link $n2 $n3 512Kb 60ms DropTail
+$ns queue-limit $n2 $n3 4
 
 
-# Code to set the queue-limit for each Links
-$ns set queue-limit $n0 $n2 10
-$ns set queue-limit $n1 $n2 10
-$ns set queue-limit $n2 $n3 10
-
-
-# Code to attach UDP Agent & Null Agent to nodes using UDP protocol(Connectionless)
-set udp0 [new Agent/UDP]
-$ns attach-agent $n0 $udp0    	/* assigning all feature of udp to n0 */ 
-
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 attach-agent $udp0
-$cbr0 set packetSize 1000
-$cbr0 set interval 0.05
-
+#Setup a UDP connection
 set udp1 [new Agent/UDP]
 $ns attach-agent $n1 $udp1
+set null1 [new Agent/Null]
+$ns attach-agent $n3 $null1
+$ns connect $udp1 $null1
+
+
+#Setup a CBR Application over UDP connection
 set cbr1 [new Application/Traffic/CBR]
 $cbr1 attach-agent $udp1
+$cbr1 set packetSize_ 500
+$cbr1 set interval_ 0.005
 
-set udp2 [new Agent/UDP]
-$ns attach-agent $n2 $udp2
-set cbr2 [new Application/Traffic/CBR]
-$cbr2 attach-agent $udp2
-
-#Assigning null object to n3
-set null0 [new Agent/Null]
-$ns attach-agent $n3 $null0
-
-
-# Code to connect the Agents
-$ns connect $udp0 $null0
-$ns connect $udp1 $null0
-
-
-# Code to start the Simulation
-$ns at 0.3 "$cbr0 start"
-$ns at 0.15 "$cbr1 start"
+$ns at 0.2 "$cbr1 start"
+$ns at 1.5 "$cbr1 stop"
 $ns at 2.0 "finish"
+
 $ns run
