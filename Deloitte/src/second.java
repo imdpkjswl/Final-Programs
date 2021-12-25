e6ab31e53d591cc926fd422c77493495ca87e20f
DEEPAK JAISWAL <43009486+deepakjaiswal2022@users.noreply.github.com>
2021-12-25 16:25:40 +0530

    Easy que asked, Took me 18mins to solve.

--- Deloitte/src/second.java
+++ Deloitte/src/second.java
@@ -1,48 +1,103 @@
 import java.util.*;
 
 public class second {
+    public static HashSet<Integer> set = new HashSet<>();
+
+    /**
+     * @param m --> Row
+     * @param n --> Column
+     * @param arr --> Square matrix
+     * @return sum
+     */
     static int diagonalSum(int m, int n, int[][] arr){
-        int s1=0;
-        int s2=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==j){
-                    s1 += arr[i][j];
-                }
-                if(i==n-j-1){
-                    s2 += arr[i][j];
-                }
+
+        for(int p=0;p<n;p++){
+            for(int q=0;q<n;q++){
+                System.out.print(arr[p][q]+" ");
             }
+            System.out.println();
+        }
+        // Store all element to sets except diagonal elements
+        for(int i=0;i<m;i++){for(int j=0;j<n;j++){
+                if(i!=j){
+                    set.add(arr[i][j]);
+                } }
         }
 
-        return Math.abs(s1-s2);
+        int sum=0;
+        for(int i=0;i<m;i++){for(int j=0;j<n;j++){
+                if(i==j) { if(set.contains(arr[i][j])){
+                        sum += arr[i][j];
+                }}
+            }
+        }
+        return sum;
     }
 
-    static int
+    static int solution(int m, int n, int[][] arr){
+        if(m==n){
+            return diagonalSum(m, n, arr);
+        }
+
+        else if(m<n){
+            int i;
+            int[][] tmp = new int[n][n]; // make square matrix
+            for(i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(i<m) {
+                        tmp[i][j] = arr[i][j];
+                    }else{
+                        tmp[i][j] = 1;
+                    }
+                }
+            }
+            return diagonalSum(n,n,tmp);
+        }
+
+        else {
+            int i;
+            int[][] tmp = new int[m][m]; // make square matrix
+            for (i = 0; i < m; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (j < n) {
+                        tmp[i][j] = arr[i][j];
+                    } else {
+                        tmp[i][j] = 1;
+                    }
+                }
+            }
+            return diagonalSum(m, m, tmp);
+        }
+
+    }
+
     public static void main(String[] args) {
         int m, n;
-        // m = 3, n = 3;
+        /*m = 3; n = 3;
         int[][] a1 = new int[][]{
                 {1,2,3},
                 {4,5,6},
-                {7,8,9}
-        };
+                {1,8,9}
+        };*/
 
-        // m = 2, n = 3;
+        //System.out.println(solution(m,n,a1));
+
+        /*m = 2; n = 3;
         int[][] a2 = new int[][]{
                 {1,2,3},
                 {4,5,6}
-        };
+        };*/
 
-        // m = 3, n = 2;
+        //System.out.println(solution(m,n,a2));
+
+        m = 3; n = 2;
         int[][] a3 = new int[][]{
                 {1,2},
                 {3,4},
-                {5,6}
+                {4,6}
         };
 
-
-
+        System.out.println(solution(m,n,a3));
 
     }
 }
